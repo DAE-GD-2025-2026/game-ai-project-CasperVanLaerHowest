8ab7d5babd515b03a4fb21003a8b9f28194274ef
Casper van Laer <caspervlaer@outlook.com>
2026-02-23 14:54:50 +0100

    Update Flee for correct movement
    Updated the flee movement so that it now correctly goes the quickest way

--- Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
+++ Source/GameAIProg/Movement/SteeringBehaviors/Steering/SteeringBehaviors.cpp
@@ -17,7 +17,8 @@ SteeringOutput Flee::CalculateSteering(float DeltaT, ASteeringAgent& Agent)
     SteeringOutput steering{};
     if ((Target.Position - Agent.GetPosition()).Size() > m_radius)
         return steering;
-    steering.LinearVelocity = Target.Position + Agent.GetPosition();
+    steering.LinearVelocity = Target.Position - Agent.GetPosition();
+    steering.LinearVelocity *= -1;
     steering.LinearVelocity.Normalize();
     steering.LinearVelocity *= Agent.GetMaxLinearSpeed();
     return steering;
